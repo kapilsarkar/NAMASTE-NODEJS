00e7a7fee5a07d3406b8e7d1e135f1d893dd595a
kapilsarkar <k.sarkar81@gmail.com>
2024-08-21 19:41:37 +0530

    updated

--- DAY-02-JS ON SERVER/index.js
+++ DAY-02-JS ON SERVER/index.js
@@ -1,9 +1,5 @@
-// Today’s lecture was all about understanding the foundation of Node.js and its incredible capabilities. 🌐
-// I learned that Node.js is not just a platform for running JavaScript on the server-side; it’s built on the V8 JavaScript engine, which is written in C++. The V8 engine is a powerhouse that converts our JavaScript code into machine code, allowing for lightning-fast execution. ⚡️
-// Exploring the detailed documentation of the V8 engine has given me a profound appreciation for its role, not only in Node.js but also in modern web browsers like Chrome. It’s fascinating to see how this seamless bridge between JavaScript and machine language enhances performance and efficiency. 🔍
-// Looking forward to diving deeper and applying what I’ve learned through hands-on coding!
-
 //DAY-02 - JS ON SERVER.
+
 
  
 
