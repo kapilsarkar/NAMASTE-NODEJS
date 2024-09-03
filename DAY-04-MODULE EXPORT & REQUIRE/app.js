225e50c4a41352dd77cf18ef18b168337fe8b210
kapilsarkar <k.sarkar81@gmail.com>
2024-09-03 08:43:20 +0530

    somepartsadded

--- DAY-04-MODULE EXPORT & REQUIRE/app.js
+++ DAY-04-MODULE EXPORT & REQUIRE/app.js
@@ -1,3 +1,19 @@
- require("./new")
-let a = "Kapil"
-console.log(a)
+require("./new.js");
+
+const obj = require("./sum.js");
+const {y, calculateTripleSum} = require("./sum.js")
+let a = "Kapil";
+console.log(a);
+
+obj.calculateSum(10, 20);
+console.log(obj.x)
+
+calculateTripleSum(10,20,30)
+console.log(y)
+
+// For ES-MODULES-
+//  import {calculateSum,x} from "./sum.js"
+
+
+//  calculateSum(100,200)
+//  console.log(x)
--- DAY-04-MODULE EXPORT & REQUIRE/sum.js
+++ DAY-04-MODULE EXPORT & REQUIRE/sum.js
@@ -1,4 +1,26 @@
-function calculateSum(a,b){
-   const sum = a + b
-   console.log(sum)
+console.log("Sum Module Executed");
+
+// export let x = "Hello Sum";
+// export function calculateSum(a, b) {
+//   const sum = a + b;
+//   console.log(sum);
+// }
+
+  let x = "Hello Sum";
+  function calculateSum(a, b) {
+   const sum = a + b;
+   console.log(sum);
+ }
+
+let y = "Hello Sum by Destructuring";
+function calculateTripleSum(a, b, c) {
+  const sum = a + b + c;
+  console.log(sum);
 }
+console.log(module.exports)
+module.exports = {
+  x,
+  y,
+  calculateSum,
+  calculateTripleSum,
+};
